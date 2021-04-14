0b7b5e22ad9411142e47e66700ba711b60321b66
fsaia <freddy.v.saia@gmail.com>
2021-04-14 18:32:01 -0400

    Update least_degreesX0.m

--- Least Degrees/X0/least_degreesX0.m
+++ Least Degrees/X0/least_degreesX0.m
@@ -179,75 +179,33 @@ end function;
 // X has infinitely many closed points of degree d.
 
 // declaring sequence
-// delta_1_X0 : sequence of all N for which delta(X_0) = 1
+// delta_1_X0 : sequence of all N for which delta(X_0(N)) = 1
 // for all of these N, X_0(N) has no sporadic CM point
 
 delta_1_X0 := [1,2,3,4,5,6,7,8,9,10,12,13,16,18,25];
 
 
 // declaring sequence
-// delta_2_X0 : sequence of all N for which delta(X_0) = 2
+// delta_2_X0 : sequence of all N for which delta(X_0(N)) = 2
 // for all of these N, X_0(N) has a sporadic CM point iff d_{CM}(X_0(N)) = 1
 
 delta_2_X0 := [11,14,15,17,19,20,21,22,23,24,26,27,28,29,30,31,32,33,35,36,37,39,40,41,43,46,47,
 48, 49, 50, 53, 59, 61, 65, 71, 79, 83, 89, 101, 131]; 
 
 
-
-// have exact gonality(X_1(N)) for N <= 40 and upper bounds for 
-// N <= 250 by Derickx, van Hoeij:
-
-// declaring sequence
-// gonality_upper_bds : Nth element is upper bound on gonality(X_1(N))
-
-gonality_upper_bds := [1,1,1,1,1,1,1,1,1,1,
-2,1,2,2,2,2,4,2,5,3,
-4,4,7,4,5,6,6,6,11,6,
-12,8,10,10,12,8,18,12,14,12,
-22,12,24,15,18,19,29,16,21,15,
-24,21,37,18,30,24,30,31,46,24,
-49,36,36,32,42,30,58,36,44,36,
-66,32,70,51,40,45,60,42,82,48,
-54,58,90,48,72,64,70,60,104,48,
-84,66,80,83,90,56,123,63,90,60,
-133,72,139,84,96,105,150,72,156,90,
-114,96,167,90,132,105,126,120,144,96,
-132,139,140,120,125,96,211,112,154,126,
-225,120,180,156,144,144,246,132,253,144,
-184,189,210,128,210,184,168,171,291,120,
-299,180,216,180,240,168,323,234,234,184,
-264,162,348,210,240,240,365,192,260,216,
-270,231,392,210,240,240,290,274,420,192,
-429,252,310,264,342,240,360,276,288,270,
-478,224,488,328,336,252,508,240,519,240,
-374,382,420,288,420,398,396,336,450,288,
-583,351,420,396,462,288,480,445,444,360,
-504,342,651,384,360,444,675,360,687,396,
-480,420,711,336,552,435,520,432,748,384,
-761,396,486,465,504,420,630,480,574,375];
-
-
-
-// Here we create list no_sporadic_CM_X0 of values of N for which we find that 
-// d_{CM}(X_0(N)) >= gonality(X_1(N)) >= gonality(X_0(N)), or for which
+// Here we create list no_sporadic_CM_X0 of values of N for which
 // we determine there is no sporadic CM point using that delta(X_0(N)) <= 2. 
 
 // generating sequence 
-// no_sporadic_CM_X0 = sequence of all N such that, using above bounds and *exact* d_{CM}(X_0(N)) 
-// calculations from dcm_X0 function, 
-// we can provably say X_0(N) has no sporadic CM point
+// no_sporadic_CM_X0 = sequence of all N such that, using exact d_{CM}(X_0(N)) 
+// calculations from dcm_X0 function, we can provably say X_0(N) has no sporadic CM point
+// via fact that delta(X_0(N)) <= 2
 
     // load "hyper_bads_X0.m";
 
-    // no_sporadic_CM_X0:= delta_1_X0;
+    // no_sporadic_CM_X0 := delta_1_X0;
 
-    // for N in [N : N in hyper_bads_X0 | (N le 250) and (not (N in delta_1_X0))] do
-    //     if dcm_X0(N)[2] ge gonality_upper_bds[N] then
-    //         Append(~no_sporadic_CM_X0, N);
-    //     end if; 
-    // end for;
-
-    // for N in [N : N in delta_2_X0 | not (N in no_sporadic_CM_X0)] do
+    // for N in delta_2_X0 do
     //     if dcm_X0(N)[2] ne 1 then
     //         Append(~no_sporadic_CM_X0, N);
     //     end if;
