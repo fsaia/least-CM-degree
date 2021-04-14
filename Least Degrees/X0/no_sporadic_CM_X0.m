a7a55cab5c3d4dbd83b57bcb33003c76f17ced31
fsaia <freddy.v.saia@gmail.com>
2021-04-14 18:33:35 -0400

    Update no_sporadic_CM_X0.m
    shortened description to remove something faulty; no change in the list

--- Least Degrees/X0/no_sporadic_CM_X0.m
+++ Least Degrees/X0/no_sporadic_CM_X0.m
@@ -1,10 +1,6 @@
 // no_sporadic_CM_X0 : sequence of the 50 values of N for which X_0(N) is shown to have no
-// sporadic CM point, either by 
-
-	// (i) using delta(X_0(N)) = 1 or 2, or
-
-	// (ii) showing that d_{CM}(X_0(N)) is at least the gonality of X_0(N), using
-	// upper bounds on gonality from Derickx and Hoeij.
+// sporadic CM point by comparing computation of dcm(X_0(N)) to delta(X_0(N)) in cases where
+// delta(X_0(N)) = 1 or 2
 
 no_sporadic_CM_X0 := [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25,
 26, 28, 29, 30, 31, 32, 33, 35, 36, 37, 39, 40, 41, 46, 47, 48, 49, 50, 53, 59, 
